9525990f34be58a9c0bb5b2ca4cf416dfc58550e
Conrado Buhrer <conrado@buhrer.net>
2017-07-02 15:39:47 -0300

    added login with firebase - step 2

--- src/components/login-form/login-form.component.ts
+++ src/components/login-form/login-form.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
 
 import { Account } from '../../models/account/account.interface';
+import { LoginResponse } from '../../models/login/login-response.interface';
 
 /**
  * Generated class for the LoginFormComponent component.
@@ -17,22 +18,30 @@ import { Account } from '../../models/account/account.interface';
 export class LoginFormComponent {
 
   account = {} as Account;
+  @Output() loginStatus: EventEmitter<LoginResponse>;
 
   constructor(private afAuth: AngularFireAuth, private navCtrl: NavController) {
+    this.loginStatus = new EventEmitter<LoginResponse>();
   }
 
   async login() {
     try {
-      const result = await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password);
-      console.log(result);
+      const result: LoginResponse = {
+        result: await this.afAuth.auth.signInWithEmailAndPassword(this.account.email, this.account.password)
+      }
+      this.loginStatus.emit(result);
     }
     catch(e) {
       console.error(e);
+      const error: LoginResponse = {
+        error: e
+      }
+      this.loginStatus.emit(error);
     }
   }
 
-  navigateToPage(pageName: string) {
-    pageName === 'TabsPage' ? this.navCtrl.setRoot(pageName) : this.navCtrl.push(pageName);
+  navigateToRegisterPage() {
+    this.navCtrl.push('RegisterPage');
   }
 
 }
