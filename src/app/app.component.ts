116acf8f8c9049dcc06f30661e87b5edd760967f
Conrado Buhrer <conrado@buhrer.net>
2017-07-18 11:47:25 -0300

    added login hold for root page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,25 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AuthProvider } from '../providers/auth/auth.provider';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:string = 'LoginPage';
+  rootPage: string;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform,
+              statusBar: StatusBar,
+              splashScreen: SplashScreen,
+              private authPrv: AuthProvider
+  ) {
+
+    this.authPrv.getAuthenticatedUser().subscribe(auth => {
+      !auth ?
+        this.rootPage = 'LoginPage':
+        this.rootPage = 'TabsPage';
+    })
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
