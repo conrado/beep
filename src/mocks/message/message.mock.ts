ac2bcc65b41c35262acdcfed680f03d1090e7297
Conrado Buhrer <conrado@buhrer.net>
2017-07-25 09:36:19 -0300

    foundation for user 1-1 chat message sending

--- src/mocks/message/message.mock.ts
+++ src/mocks/message/message.mock.ts
@@ -6,9 +6,9 @@ import { PROFILE_LIST } from '../profile/profile.mock'
 const userList = PROFILE_LIST;
 const messageList: Message[] = [];
 
-userList.forEach(user => {
-  messageList.push({user: user, date: new Date(), lastMessage: 'Hello'})
-  messageList.push({user: user, date: new Date(), lastMessage: 'Hello'})
-})
+//userList.forEach(user => {
+  //messageList.push({user: user, date: new Date(), lastMessage: 'Hello'})
+  //messageList.push({user: user, date: new Date(), lastMessage: 'Hello'})
+//})
 
 export const MESSAGE_LIST = messageList;
--- src/pages/message/message.ts
+++ src/pages/message/message.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { AuthProvider } from '../../providers/auth/auth.provider';
+
 import { Profile } from '../../models/profile/profile.interface';
 import { Message } from '../../models/message/message.interface';
 
@@ -14,15 +16,21 @@ import { MESSAGE_LIST } from '../../mocks/message/message.mock';
 export class MessagePage {
 
   selectedProfile: Profile;
-
   messageList: Message[];
+  userId: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private navCtrl: NavController,
+    private navParams: NavParams,
+    private authPrv: AuthProvider
+  ) {
     this.messageList = MESSAGE_LIST;
   }
 
   ionViewWillLoad() {
     this.selectedProfile = this.navParams.get('profile');
+    this.authPrv.getAuthenticatedUser()
+      .subscribe(auth => this.userId = auth.uid);
   }
 
 }
